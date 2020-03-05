0dbccd8749227cc673dedbf10a2b44655e2ffccc
vishalgoney20 <vishal.goney@zemosolabs.com>
2020-03-05 15:31:52 +0530

    Assignment_1 modified

--- java/1/src/FileRegularExpression.java
+++ java/1/src/FileRegularExpression.java
@@ -1,21 +1,19 @@
 import java.io.File;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.*;
 
 public class FileRegularExpression {
-    public void findFilePath(String path, String expression) throws NullPointerException {
-        try {
-            File folder = new File(path);
-            for (File file : folder.listFiles()) {
-                if (file.isDirectory())
-                    findFilePath(file.getAbsolutePath(), expression);
-                if (Pattern.matches(expression, file.getName()))
-                    System.out.println(file.getAbsolutePath());
+    public void findFilePath(File path, String expression) throws NullPointerException {
+        if(path.listFiles() == null)
+            return;
+        for (File file : path.listFiles()) {
+            if(file.isDirectory()) {
+                findFilePath(file, expression);
             }
+            else if (Pattern.matches(expression, file.getName()))
+                System.out.println(file.getAbsolutePath());
         }
-        catch(Exception e){
-            System.out.println(e);
-        }
+
     }
 
     public static void main(String[] args) {
@@ -28,7 +26,8 @@ public class FileRegularExpression {
             if (expression == "q")
                 break;
             else
-                file.findFilePath("/home/", expression);
+                file.findFilePath(new File(System.getProperty("user.home")), expression);
         }
     }
 }
+//  .*/*.java
