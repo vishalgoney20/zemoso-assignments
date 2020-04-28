56deee2c7f4b759b4b147dec70e7cc4e8229620c
vishal <vishal.goney@zemosolabs.com>
2020-04-28 11:01:37 +0530

    Rest Assignment

---
+++ REST/capstone challenge 4.2/SharingApp/app/src/main/java/com/example/sharingapp/AddBidCommand.java
@@ -0,0 +1,57 @@
+package com.example.sharingapp;
+
+import android.content.Context;
+
+/**
+ * Command to add a bid
+ */
+public class AddBidCommand extends Command {
+
+
+
+    
+    private Bid bid;
+    
+
+    public AddBidCommand( Bid bid) {
+        this.bid = bid;
+        }
+
+    // Save bid remotely
+    public void execute(){
+
+       ElasticSearchManager.AddBidTask add_bid = new ElasticSearchManager.AddBidTask();
+        add_bid.execute(bid);
+
+        try {
+            if(add_bid.get()) {
+                super.setIsExecuted(true);
+            }
+        } 
+	catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            super.setIsExecuted(false);
+        }
+    }
+}
+
+
+/*  To save bid locally
+
+    private BidList bid_list;
+    private Bid bid;
+    private Context context;
+
+    public AddBidCommand(BidList bid_list, Bid bid, Context context) {
+        this.bid_list = bid_list;
+        this.bid = bid;
+        this.context = context;
+    }
+
+    // Save bid locally
+    public void execute(){
+        bid_list.addBid(bid);
+        super.setIsExecuted(bid_list.saveBids(context));
+    }
+*/
+
---
+++ REST/capstone challenge 4.2/SharingApp/app/src/main/java/com/example/sharingapp/DeleteBidCommand.java
@@ -0,0 +1,54 @@
+package com.example.sharingapp;
+
+import android.content.Context;
+
+/**
+ * Command to delete a bid
+ */
+public class DeleteBidCommand extends Command {
+
+
+	 
+    private Bid bid;
+    public DeleteBidCommand(Bid bi) {
+      
+	 this.bid = bid;
+    }
+
+    // Delete bid remotely
+    public void execute(){
+       ElasticSearchManager.RemoveBidTask remove_bid = new ElasticSearchManager.RemoveBidTask();
+        Remove_bid.execute(bid);
+
+        try {
+            if(remove_bid.get()) {
+                super.setIsExecuted(true);
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            super.setIsExecuted(false);
+        }
+       
+    }
+    
+}
+
+
+/*  To delete bid locally
+
+private BidList bid_list;
+    private Bid bid;
+    private Context context;
+
+    public DeleteBidCommand(BidList bid_list, Bid bid, Context context) {
+        this.bid_list = bid_list;
+        this.bid = bid;
+        this.context = context;
+    }
+
+    // Delete bid locally
+    public void execute(){
+        bid_list.removeBid(bid);
+        super.setIsExecuted(bid_list.saveBids(context));
+    }
+*/
